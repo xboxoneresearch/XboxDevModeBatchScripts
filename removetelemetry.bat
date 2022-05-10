ebba7894c3b80d2974e4e2ec3725719dff94182c
TitleOS <titleos@titleos.dev>
2022-05-10 14:05:50 -0500

    Added WerSvc to the telemetry service list

--- removetelemetry.bat
+++ removetelemetry.bat
@@ -1,9 +1,11 @@
-:: Stops then deletes the four primary telemetry services on Xbox SystemOS. Requires Admin or above.
+:: Stops then deletes the five primary telemetry services on Xbox SystemOS. Requires Admin or above.
 sc stop etwuploaderservice
 sc stop DiagTrack
 sc stop XBBlackbox
 sc stop xbdiagservice
+sc stop wersvc
 sc delete etwuploaderservice
 sc delete DiagTrack
 sc delete XBBlackbox
 sc delete xbdiagservice
+sc delete wersvc
