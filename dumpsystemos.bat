9fec3190d2d6e0c8a382592c46e7f1e1d7710fd9
TitleOS <titleos@titleos.dev>
2022-05-15 08:10:37 -0500

    Updated dumpsystemos.bat to use SystemOS version
    Updated dumpsystemos.bat to parse VER for the current SystemOS build, which is then used for the root dumping folder.

--- dumpsystemos.bat
+++ dumpsystemos.bat
@@ -1,16 +1,20 @@
-rmdir D:\DevelopmentFiles\sysosdump /s /q
-mkdir D:\DevelopmentFiles\sysosdump
-mkdir D:\DevelopmentFiles\sysosdump\CDrive
-mkdir D:\DevelopmentFiles\sysosdump\XDrive
-mkdir D:\DevelopmentFiles\sysosdump\YDrive
-mkdir D:\DevelopmentFiles\sysosdump\SDrive
-mkdir D:\DevelopmentFiles\sysosdump\JDrive
-mkdir D:\DevelopmentFiles\sysosdump\MDrive
-mkdir D:\DevelopmentFiles\sysosdump\NDrive
-xcopy C:\ D:\DevelopmentFiles\sysosdump\CDrive /s /e /h /y
-xcopy X:\ D:\DevelopmentFiles\sysosdump\XDrive /s /e /h /y
-xcopy Y:\ D:\DevelopmentFiles\sysosdump\YDrive /s /e /h /y
-xcopy S:\ D:\DevelopmentFiles\sysosdump\SDrive /s /e /h /y
-xcopy J:\ D:\DevelopmentFiles\sysosdump\JDrive /s /e /h /y
-xcopy M:\ D:\DevelopmentFiles\sysosdump\MDrive /s /e /h /y
-xcopy N:\ D:\DevelopmentFiles\sysosdump\NDrive /s /e /h /y
+::Get and parse current SystemOS version to create root dump folder.
+for /f "tokens=2 delims=[]" %%x in ('ver') do set WINVER=%%x
+set WINVER=%WINVER:Version =%
+mkdir D:\DevelopmentFiles\%WINVER%
+
+::Dump SystemOS from select drives, excluding things like the user xvd.
+mkdir D:\DevelopmentFiles\%WINVER%\CDrive
+mkdir D:\DevelopmentFiles\%WINVER%\XDrive
+mkdir D:\DevelopmentFiles\%WINVER%\YDrive
+mkdir D:\DevelopmentFiles\%WINVER%\SDrive
+mkdir D:\DevelopmentFiles\%WINVER%\JDrive
+mkdir D:\DevelopmentFiles\%WINVER%\MDrive
+mkdir D:\DevelopmentFiles\%WINVER%\NDrive
+xcopy C:\ D:\DevelopmentFiles\%WINVER%\CDrive /s /e /h /y
+xcopy X:\ D:\DevelopmentFiles\%WINVER%\XDrive /s /e /h /y
+xcopy Y:\ D:\DevelopmentFiles\%WINVER%\YDrive /s /e /h /y
+xcopy S:\ D:\DevelopmentFiles\%WINVER%\SDrive /s /e /h /y
+xcopy J:\ D:\DevelopmentFiles\%WINVER%\JDrive /s /e /h /y
+xcopy M:\ D:\DevelopmentFiles\%WINVER%\MDrive /s /e /h /y
+xcopy N:\ D:\DevelopmentFiles\%WINVER%\NDrive /s /e /h /y
